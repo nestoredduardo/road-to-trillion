3728e78286241df67f59c3395ce2e233c3f71b79
Nestor Eduardo Mamani Pantoja <nmamanipantoja@gmail.com>
2021-10-26 00:11:48 +0000

    ⚔️ Building the Arena page.

--- src/Components/Arena/index.js
+++ src/Components/Arena/index.js
@@ -4,8 +4,10 @@ import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
 import myEpicGame from '../../utils/MyEpicGame.json';
 import './Arena.css';
 
-const Arena = ({ characterNFT }) => {
+const Arena = ({ characterNFT, setCharacterNFT }) => {
   const [gameContract, setGameContract] = useState(null);
+  const [boss, setBoss] = useState(null);
+  const [attackState, setAttackState] = useState('');
 
   useEffect(() => {
     const { ethereum } = window;
@@ -25,13 +27,101 @@ const Arena = ({ characterNFT }) => {
     }
   }, []);
 
+  useEffect(()=>{
+    const fetchBoss = async () => {
+      const bossTxn = await gameContract.getBigBoss();
+      console.log('Boss: ', bossTxn);
+      setBoss(transformCharacterData(bossTxn));
+    }
+
+    const onAttackComplete = (newBossHp, newPlayerHp) =>{
+      const bossHp = newBossHp.toNumber();
+      const playerHp = newPlayerHp.toNumber();
+
+      console.log(`AttackComplete: Boss Hp: ${bossHp} Player Hp: ${playerHp}`)
+
+      setBoss((prevState) => {
+        return { ...prevState, hp:bossHp}
+      })
+
+      setCharacterNFT((prevState)=>{
+        return { ...prevState, hp: playerHp}
+      })
+    }
+
+    if(gameContract){
+      fetchBoss();
+      gameContract.on('AttackComplete', onAttackComplete);
+    }
+
+    return () => {
+      if (gameContract) {
+          gameContract.off('AttackComplete', onAttackComplete);
+      }
+    }
+  }, [gameContract])
+
+  const runAttackAction = async () => {
+    try{
+      if(gameContract){
+        setAttackState('attacking');
+        console.log('Attacking Boss...');
+        const attackTxn = await gameContract.attackBoss();
+        await attackTxn.wait();
+        console.log('attackTxn:', attackTxn);
+        setAttackState('hit');
+      }
+    }catch(error){
+      console.error('Error attacking the boss:', error)
+      setAttackState('');
+    }
+  };
+
   return (
     <div className="arena-container">
-      {/* Boss */}
-      <p>BOSS GOES HERE</p>
+      {boss && (
+        <div className="boss-container">
+          <div className={`boss-content ${attackState}`}>
+            <h2>🔥 {boss.name} 🔥</h2>
+            <div className="image-content">
+              <img src={boss.imageURI} alt={`Boss ${boss.name}`} />
+              <div className="health-bar">
+                <progress value={boss.hp} max={boss.maxHp} />
+                <p>{`${boss.hp} / ${boss.maxHp} HP`}</p>
+              </div>
+            </div>
+          </div>
+          <div className="attack-container">
+            <button className="cta-button" onClick={runAttackAction}>
+              {`💥 Attack ${boss.name}`}
+            </button>
+          </div>
+        </div>
+      )}
 
-      {/* Character NFT */}
-      <p>CHARACTER NFT GOES HERE</p>
+      {characterNFT && (
+        <div className="players-container">
+          <div className="player-container">
+            <h2>Your Character</h2>
+            <div className="player">
+              <div className="image-content">
+                <h2>{characterNFT.name}</h2>
+                <img
+                  src={characterNFT.imageURI}
+                  alt={`Character ${characterNFT.name}`}
+                />
+                <div className="health-bar">
+                  <progress value={characterNFT.hp} max={characterNFT.maxHp} />
+                  <p>{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</p>
+                </div>
+              </div>
+              <div className="stats">
+                <h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
